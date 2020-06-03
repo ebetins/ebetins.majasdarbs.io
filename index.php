78f9c0c484eaa7705668e218a47bd9bc7a68db14
ebetins <esildere@gmail.com>
2020-06-03 21:24:39 +0300

    Add main files

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href = "./libs/bootstrap-3.3.7/dist/css/bootstrap.css" rel ="stylesheet">
+    <link href="./css/main.css" rel="stylesheet">
+    <script src="./libs/jQuery/jquery.js"></script>
+    <script src="./js/my.js"></script>
+    <script src="./libs/bootstrap-3.3.7/dist/js/bootstrap.js"> </script>
+    <title>Document</title>
+    <script>
+        $("document").ready(function () {
+          
+        });
+    </script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><img src="" alt=""></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav  navbar-right">
+                <li  class="active"><a href="#">Jaunumi</a></li>
+                <li><a href="">Kontakti</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="contaier-fluid" id="">
+    <div class="row">
+		<div class = "col-md-2">
+			<p>Piemers</p>
+		</div>
+		<div class="col-md-8">
+			<div class="row">
+				<div class="col-md-3 col-sm-6">A</div>
+				<div class="col-md-3 col-sm-6">B</div>
+				<div class="col-md-3 col-sm-6">C</div>
+				<div class="col-md-3 col-sm-6">D</div>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<p>Piemers</p>
+		</div>
+    </div>
+</div>
+</body>
+</html>
